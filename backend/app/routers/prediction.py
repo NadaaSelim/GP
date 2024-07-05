5bd24347a480568ba2a16cf923bf8907e0be0269
Lama-Khaled-Ismail <lamakhaledismail@gmail.com>
2024-07-05 21:01:48 +0300

    added monthly summary

--- backend/app/routers/prediction.py
+++ backend/app/routers/prediction.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+from datetime import datetime
 from fastapi import APIRouter, Depends, HTTPException, status
 from mongo.database import english_collection
 import models, oauth2, schemas
@@ -11,7 +13,7 @@ from sqlalchemy.orm import Session
 
 
 router = APIRouter(
-    prefix="/predict",
+    prefix="/predict/en",
     tags=["prediction"],
 )
 
@@ -31,7 +33,7 @@ def predict():
     return{"score":sentiment_label}
 
 
-@router.post("/en/{brand_name}/{platform}", status_code=status.HTTP_201_CREATED, response_model=schemas.AnalysisOut)
+@router.post("/{brand_name}/{platform}", status_code=status.HTTP_201_CREATED, response_model=schemas.AnalysisOut)
 def predict_en(brand_name: str, platform: str, db:Session = Depends(get_db), current_user: int =Depends(oauth2.get_current_user)):
     try:
         brand = db.query(models.Brand).filter(models.Brand.name==brand_name).first()
@@ -103,3 +105,45 @@ def predict_en(brand_name: str, platform: str, db:Session = Depends(get_db), cur
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+@router.get("/{brand_name}/{platform}")
+def analysis_by_month(brand_name: str, platform: str, db:Session = Depends(get_db), current_user: int =Depends(oauth2.get_current_user)):
+    try:
+        brand = db.query(models.Brand).filter(models.Brand.name==brand_name).first()
+        # Fetch data from MongoDB
+        documents = list(english_collection.find({ 'brand_id': brand.id ,'platform': platform},{'_id': 1,'text': 1, 'time': 1}))
+        
+        if not documents:
+            raise HTTPException(status_code=404, detail="No documents found in the collection")
+
+        # Convert MongoDB documents to DataFrame
+        df = pd.DataFrame(documents)
+        
+        # Ensure the DataFrame has the required features
+        if not all(feature in df.columns for feature in features):
+            raise HTTPException(status_code=400, detail=f"Input data must contain the following features: {features}")
+
+        # Prepare the data for prediction
+        X_batch = df['text']
+       
+
+        # Make predictions
+        predictions = ml_models["en"].predict(X_batch)
+        
+        monthly_summary = defaultdict(lambda: {"positive": 0, "negative": 0})
+        # Update MongoDB documents with predictions
+        for doc, prediction in zip(documents, predictions):
+            date_obj = datetime.strptime(doc["time"], "%d %B %Y")
+            month_str = date_obj.strftime("%B")
+            
+            if prediction == 1:
+                monthly_summary[month_str]["positive"] += 1
+            else:
+                monthly_summary[month_str]["negative"] += 1
+               
+                
+            english_collection.update_one({'_id':  doc['_id']}, {'$set': {'score': int(prediction)}})
+            
+        return monthly_summary
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
