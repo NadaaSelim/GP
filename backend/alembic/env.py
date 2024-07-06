baa24cf38277100d16b5d7fe9a448db26656300c
Lama-Khaled-Ismail <lamakhaledismail@gmail.com>
2024-07-06 12:34:19 +0300

    added in requirments.txt

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -4,11 +4,12 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from  app.models import Base
+from  app.postgres.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
